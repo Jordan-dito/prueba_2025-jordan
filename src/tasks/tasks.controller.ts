3a227f1339051aa11320da4ef03adf01e98f7f82
Jordan-dito <75227714+Jordan-dito@users.noreply.github.com>
2025-01-29 13:24:00 -0500

    mejorar configuración de Swagger

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,42 +6,48 @@ import {
   Delete,
   Param,
   Body,
-  UseGuards,
+  // UseGuards, // Comentado para deshabilitar la validación de token
 } from '@nestjs/common';
+import { ApiBody, ApiParam } from '@nestjs/swagger';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+// import { JwtAuthGuard } from '../auth/jwt-auth.guard'; // Comentado para deshabilitar la validación de token
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @UseGuards(JwtAuthGuard) // 🔐 Protege el endpoint con JWT
+  // @UseGuards(JwtAuthGuard) // Comentado para deshabilitar la validación de token
   @Get()
   findAll(): Promise<Task[]> {
     return this.tasksService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @Get(':id')
+  @ApiParam({ name: 'id', type: Number })
   findOne(@Param('id') id: number): Promise<Task> {
     return this.tasksService.findOne(id);
   }
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @Post()
+  @ApiBody({ type: Task })
   create(@Body() task: Partial<Task>): Promise<Task> {
     return this.tasksService.create(task);
   }
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @Put(':id')
+  @ApiParam({ name: 'id', type: Number })
+  @ApiBody({ type: Task })
   update(@Param('id') id: number, @Body() task: Partial<Task>): Promise<Task> {
     return this.tasksService.update(id, task);
   }
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @Delete(':id')
+  @ApiParam({ name: 'id', type: Number })
   delete(@Param('id') id: number): Promise<void> {
     return this.tasksService.delete(id);
   }
